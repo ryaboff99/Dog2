494ad88bcdd50ad9ef353deb6d91c2a94f326414
ryaboff99 <ryaboff99@gmail.com>
2018-07-24 20:38:47 +0300

    Dog2 & Dog2TestDrive

---
+++ src/Dog2.java
@@ -0,0 +1,18 @@
+public class Dog2 {
+    public static void main(String[] args) {
+        int size;
+        String name;
+
+        void bark() {
+            if (size > 60) {
+                System.out.println("Woof! Woof!");
+            } else if (size > 14) {
+                System.out.println("Ruff! Ruff!");
+            } else {
+                System.out.println("Yip! Tip!")
+            }
+        }
+    }
+
+}
+
